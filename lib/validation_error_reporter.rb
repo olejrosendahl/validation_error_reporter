9c0bd7d74722737ac39d10fe52eebf779cad487c
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-08-23 13:38:23 +0700

    Removed unused code

--- lib/validation_error_reporter.rb
+++ lib/validation_error_reporter.rb
@@ -1,7 +1,5 @@
 require "validation_error_reporter/version"
 require "validation_error_reporter/plaintext_formatter"
-require "validation_error_reporter/error_finder"
-require "validation_error_reporter/options_validator"
 require "validation_error_reporter/model_resolver"
 require "validation_error_reporter/error_report"
 require "validation_error_reporter/runner"
