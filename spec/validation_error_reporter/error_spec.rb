4a67466daca6bc465e103c30182dbd7f2d569b1f
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-08 18:35:38 +0700

    Added tests for ProfileFormatter and Error.

--- spec/validation_error_reporter/error_spec.rb
+++ spec/validation_error_reporter/error_spec.rb
@@ -1,14 +1,25 @@
 require "spec_helper"
 
 describe ValidationErrorReporter::Error do
+  subject do
+    described_class.new(double(
+      id: 1,
+      class: double(
+        model_name: double(name: "Project", human: "Project"),
+        count: 1,
+        primary_key: :id
+      ),
+      errors: double(
+        full_messages: [
+          "Name can't be blank",
+          "Name is too short (minimum is 2 characters)"]
+      )
+    ))
+  end
 
   describe "#to_s" do
     it "outputs the records with the errors" do
-      klass = double(model_name: double(human: "Project"), primary_key: :id)
-      errors = double(full_messages: ["Name can't be blank", "Name is too short (minimum is 2 characters)"])
-      project = double(class: klass, id: 1, errors: errors)
-
-      expect(described_class.new(project).to_s).to eq(
+      expect(subject.to_s).to eq(
         "Project 1:\n" +
         "  Name can't be blank\n" +
         "  Name is too short (minimum is 2 characters)"
@@ -17,8 +28,14 @@ describe ValidationErrorReporter::Error do
   end
 
   describe "#model_name" do
+    it "returns the name of the wrapped model record" do
+      expect(subject.model_name).to eq("Project")
+    end
   end
 
   describe "#model_total_count" do
+    it "return the total number of records for the model" do
+      expect(subject.model_total_count).to eq(1)
+    end
   end
 end
---
+++ spec/validation_error_reporter/profile_formatter_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe ValidationErrorReporter::ProfileFormatter do
+  let(:profile) {{
+    "Project" => { count: 1, total: 1, rate: 100.0 }
+  }}
+
+  describe "#format(profile)" do
+    it "creates a summary string from the given hash" do
+      expect(subject.format(profile)).to eq(
+        "Summary:\n" +
+        "Model with most errors: Project (1 entries, 1 errors, 100.0%).\n" +
+        "Model with highest error rate: Project (1 entries, 1 errors, 100.0%)."
+      )
+    end
+
+    describe "when profile is empty" do
+      it "returns 'No errors'" do
+        expect(subject.format({})).to eq("No errors")
+      end
+    end
+  end
+end
